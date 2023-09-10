8922b1edf4b82d9f2f15aee7cedec8df2acccda1
Dimid19 <dimapotapenko432@gmail.com>
2023-09-10 18:34:20 +0000

    Version1.0

--- lesson1.py
+++
@@ -1,1 +0,0 @@
-THe first Lesson python code
---
+++ palindom.py
@@ -0,0 +1,9 @@
+from typing import Union
+
+user_input = input("Add string and number: ")
+
+def is_palindrome(origin: Union[str, int]) -> bool:
+    origin = str(origin)
+    length = len(origin)
+
+   
