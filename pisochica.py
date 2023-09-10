193ed3ada3b86fa42d2488491c78670fecdc13a1
Dimid19 <dimapotapenko432@gmail.com>
2023-09-10 20:34:38 +0000

    Create fun for the longest_palindrom

---
+++ pisochica.py
@@ -0,0 +1,12 @@
+def get_longest_palindrome(origin: str) -> str:
+    longest_palindrome = ""
+
+    for i in range(len(origin)):
+        for j in range(i + 1, len(origin) + 1):
+            substring = origin[i:j]
+            if substring == substring[::-1] and len(substring) > len(longest_palindrome):
+                longest_palindrome = substring
+
+    return longest_palindrome
+
+
