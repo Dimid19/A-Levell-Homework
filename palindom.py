79b7db544568e2ddcb3e19ad15242ae100d16720
Dimid19 <82875925+Dimid19@users.noreply.github.com>
2023-09-11 00:05:57 +0300

    Revert "Homevork palindrome"

--- palindom.py
+++ palindom.py
@@ -1,15 +1,8 @@
 from typing import Union
+
 user_input = input("Add string and number: ")
+
 def is_palindrome(origin: Union[str, int]) -> bool:
     origin = str(origin)
     length = len(origin)
-    for i in range(length // 2):
-        if origin[i] != origin[length - 1 - i]:
-            return False
-    return  True
-result = is_palindrome(user_input)
-if result:
-    print(f"{user_input} - Is pal.")
-else:
-    print(f"{user_input} - Is not pal.")
 
